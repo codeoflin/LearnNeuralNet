fa1bc43b6769e792377ebd7b86cf761e3d330e25
codeoflinz <g@hhh.im>
2019-01-07 17:07:39 +0800

    加了随机数种子

--- MyNet.py
+++ MyNet.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+import time
+import datetime
 
 # region 常量
 # 求导数的时候用的极小值
@@ -10,6 +12,16 @@ H = 1e-4
 # region 激活函数
 def Sigmoid(x):
     return 1/(1+np.exp(-x))
+
+def Sigmoid_grad(x):
+    return (1.0 - Sigmoid(x)) * Sigmoid(x)
+
+# Softmax
+def Softmax(a):
+    c=np.max(a)
+    exp_a = np.exp(a-c)
+    y = exp_a / np.sum(exp_a)
+    return y
 
 # 恒等函数
 def indentity_func(x):
@@ -66,12 +78,11 @@ class Net:
     # 这里初始化一个W,B网络
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.network = {}
-        self.network["W1"] = weight_init_std / \
-            np.random.rand(input_size, hidden_size)
+        np.random.seed(int(time.time()))
+        self.network["W1"] = weight_init_std / np.random.rand(input_size, hidden_size)
         self.network["B1"] = np.zeros(hidden_size)
 
-        self.network["W2"] = weight_init_std / \
-            np.random.rand(hidden_size, output_size)
+        self.network["W2"] = weight_init_std / np.random.rand(hidden_size, output_size)
         self.network["B2"] = np.zeros(output_size)
 
     # 前向运算
@@ -83,7 +94,7 @@ class Net:
         z1 = Sigmoid(np.dot(x, w1)+b1)
 
         # 输出层
-        y = indentity_func(np.dot(z1, w2)+b2)
+        y = Softmax(np.dot(z1, w2)+b2)
         return y
     # endregion
 
@@ -114,17 +125,42 @@ class Net:
         grads['B2'] = numerical_gradient(loss_W, self.network['B2'])
         return grads
 
+    # 反向传播
+    def gradient(self, x, t):
+        W1, W2 = self.network['W1'], self.network['W2']
+        b1, b2 = self.network['B1'], self.network['B2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = Sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = Softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['B2'] = np.sum(dy, axis=0)
+        
+        da1 = np.dot(dy, W2.T)
+        dz1 = Sigmoid(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['B1'] = np.sum(dz1, axis=0)
+
+        return grads
+    # endregion
+    
     def gradient_desent(self, x, t, lr=1, step_num=100):
         for i in range(step_num):
-            grad = self.numerical_gradient(x, t)
+            grad = self.gradient(x, t)
             self.network['W1'] -= grad['W1']*lr
             self.network['B1'] -= grad['B1']*lr
             self.network['W2'] -= grad['W2']*lr
             self.network['B2'] -= grad['B2']*lr
         pass
-
-    # endregion
-
+    
 def _change_one_hot_label(X):
     T = np.zeros((X.size, 2))
     for idx, row in enumerate(T):
@@ -135,19 +171,23 @@ def _change_one_hot_label(X):
 # 测试网络
 def nettest():
     # 生成0~30之间的数字
-    x = np.arange(0,30,1).reshape(30,1)
+    x = np.arange(0,1,0.001)
+    x = x.reshape(x.size,1)
 
     # 计算它的 单/双 以onehot格式输出
-    t =_change_one_hot_label(np.remainder(x,2))
+    # t =_change_one_hot_label(np.remainder(x,2))
+
+    # 
+    t =_change_one_hot_label((x>0.05).astype(np.int))
 
     # 准备神经网络
-    net = Net(1, 4, 2)
+    net = Net(1, 1, 2)
 
     # 训练前输出效果
     print(net.accuracy(x,t))
 
     # 训练
-    net.gradient_desent(x,t,0.1,1000)
+    net.gradient_desent(x,t,1,1000)
 
     # 训练后输出效果
     print(net.accuracy(x,t))
