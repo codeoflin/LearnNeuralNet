6c49ec6200303d179396634572af481a74b2592d
codeoflinz <g@hhh.im>
2019-01-07 15:49:02 +0800

    修改后

--- MyNet.py
+++ MyNet.py
@@ -2,78 +2,153 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-#region 常量
+# region 常量
 # 求导数的时候用的极小值
-H=1e-4
-#endregion
+H = 1e-4
+# endregion
+
+# region 激活函数
+def Sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+# 恒等函数
+def indentity_func(x):
+    return x
+# endregion 激活函数
+
+
+# region 损失函数 参数:神经网络输出,正确数据
+# 平方差
+def Mean_Square_Error(y, t):
+    return 0.5*np.sum((y-t)**2)
+
+# 交叉熵
+def Cross_Entropy_Error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    return 0-np.sum(t * np.log(y + 1e-7)) / batch_size
+# endregion
+
+# region 偏导数 算法
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 还原值
+    return grad
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+# endregion
 
 class Net:
-#region 网络
-    #region 激活函数
-    @staticmethod
-    def Sigmoid(x):
-        return 1/(1+np.exp(-x))
-
-    # 恒等函数
-    
-    @staticmethod
-    def indentity_func(x):
-        return x
-    #endregion 激活函数
-
+    # region 网络
     # 这里初始化一个W,B网络
-    def __init__(self):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.network = {}
-        self.network["W1"] = np.random.rand(1)
-        self.network["B1"] = np.random.rand(1)
+        self.network["W1"] = weight_init_std / \
+            np.random.rand(input_size, hidden_size)
+        self.network["B1"] = np.zeros(hidden_size)
 
-        self.network["W2"] = np.random.rand(1,2)
-        self.network["B2"] = np.random.rand(1,2)
+        self.network["W2"] = weight_init_std / \
+            np.random.rand(hidden_size, output_size)
+        self.network["B2"] = np.zeros(output_size)
 
-    # 前向运算
-    def forward(self, x):
+    # region 前向运算
+    def predict(self, x):
         w1, w2 = self.network["W1"], self.network["W2"]
         b1, b2 = self.network['B1'], self.network['B2']
 
         # 输入层
-        z1 = Net.Sigmoid(np.dot(x, w1)+b1)
+        z1 = Sigmoid(np.dot(x, w1)+b1)
 
         # 输出层
-        y = Net.indentity_func(np.dot(z1, w2)+b2)
+        y = indentity_func(np.dot(z1, w2)+b2)
         return y
-    #endregion
+    # endregion
 
-    #region 损失函数 参数:神经网络输出,正确数据
-    # 平方差
-    @staticmethod
-    def Mean_Square_Error(y,t):
-        return 0.5*np.sum((y-t)**2)
-        
-    # 交叉熵
-    @staticmethod
-    def Cross_Entropy_Error(y, t):
-        if y.ndim == 1:
-            t = t.reshape(1, t.size)
-            y = y.reshape(1, y.size)
-        batch_size = y.shape[0]
-        return 0-np.sum(t * np.log(y + 1e-7)) / batch_size
+    # region 损失函数 x:输入数据, t:监督数据
+    def loss(self, x, t):
+        y = self.predict(x)
+        return Cross_Entropy_Error(y, t)
+    # endregion
 
+    # region 正确率
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+    # endregion
+
+    # region 梯度下降
+    # 梯度生成算法
     # x:输入数据, t:监督数据
-    def loss(self,x, t):
-        y = self.forward(x)
-        return Net.Cross_Entropy_Error(y, t)
-    #endregion
+    def numerical_gradient(self, x, t):
+        def loss_W(W): return self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.network['W1'])
+        grads['B1'] = numerical_gradient(loss_W, self.network['B1'])
+        grads['W2'] = numerical_gradient(loss_W, self.network['W2'])
+        grads['B2'] = numerical_gradient(loss_W, self.network['B2'])
+        return grads
 
-    #region 梯度运算
+    def gradient_desent(self, x, t, lr=1, step_num=100):
+        for i in range(step_num):
+            grad = self.numerical_gradient(x, t)
+            self.network['W1'] -= grad['W1']*lr
+            self.network['B1'] -= grad['B1']*lr
+            self.network['W2'] -= grad['W2']*lr
+            self.network['B2'] -= grad['B2']*lr
+        pass
 
-    #endregion
+    # endregion
+
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 2))
+    for idx, row in enumerate(T):
+        row[X[idx]] = 1
+        
+    return T
 
 # 测试网络
 def nettest():
-    net=Net()
-    x=np.array([0])
-    y=net.forward(x)
-    print(y)
+    net = Net(1, 1, 2)
+    x = np.arange(0,30,1)
+    x = x.reshape(x.size,1)
+    t =_change_one_hot_label(np.remainder(x,2))
+
+    # 训练前输出效果
+    print(net.accuracy(x,t))
+
+    net.gradient_desent(x,t,0.01,100)
+
+    # 训练后输出效果
+    print(net.accuracy(x,t))
+    #y = net.predict(x)
+    #print(y)
+
 
 if __name__ == "__main__":
     nettest()
